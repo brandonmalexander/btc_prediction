e364ada5748b963eb34f0cf1d58505b63870e98c
brandonmalexander <32103212+brandonmalexander@users.noreply.github.com>
2018-02-15 16:16:25 -0500

    Init repo

---
+++ src/analyze_sentiment.py
@@ -0,0 +1,29 @@
+from textblob import TextBlob
+import pandas as pd
+import re
+
+
+def strip_non_ascii(string):
+    ''' Returns the string without non ASCII characters'''
+    stripped = (c for c in string if 0 < ord(c) < 127)
+    return ''.join(stripped)
+
+
+def clean_text(s):
+    """ Remove non alphabetic characters. E.g. 'B:a,n+a1n$a' becomes 'Banana' """
+
+    s = re.sub("[^a-z A-Z]", "", s)
+    s = s.replace(' n ', ' ')
+    return s
+
+
+read = 'data/reddit_raw_data.csv'
+
+rawtextdata = pd.read_csv(filepath_or_buffer=read, infer_datetime_format=True)
+rawtextdata = rawtextdata.set_index(rawtextdata.columns[0])
+
+cleanedtextdata = rawtextdata.fillna('0')
+
+sentimentdata = cleanedtextdata.applymap(lambda x: TextBlob(x).sentiment.polarity)
+
+sentimentdata.to_csv(path_or_buf='data/reddit_sentiment_data.csv')
