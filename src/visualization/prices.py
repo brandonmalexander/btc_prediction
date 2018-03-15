d97db1a4740746f65d734ce67ee41b9942c42e84
brandon <alexander.bra@husky.neu.edu>
2018-03-15 18:57:07 -0400

    updated prices.py and added prices.png

---
+++ src/visualization/prices.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+read_from = '/home/brandon/Northeastern/1b_Spring18/EECE_2300/3_15_2lmao/btc_prediction/src/data/price_data.csv'
+prices = pd.read_csv(filepath_or_buffer=read_from, infer_datetime_format=True)
+prices = prices.set_index(prices.columns[0])
+prices = prices.set_index(pd.DatetimeIndex(prices.index).to_pydatetime())
+
+plt.plot(prices.index, prices.price)
+
+plt.savefig('prices.png')
+
