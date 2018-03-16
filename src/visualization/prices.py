b8f8c7e352b5982b54786de974b8751bc4a55993
brandon <alexander.bra@husky.neu.edu>
2018-03-15 20:53:39 -0400

    same as last timeagain

--- src/visualization/prices.py
+++ src/visualization/prices.py
@@ -6,8 +6,7 @@ prices = pd.read_csv(filepath_or_buffer=read_from, infer_datetime_format=True)
 prices = prices.set_index(prices.columns[0])
 prices = prices.set_index(pd.DatetimeIndex(prices.index).to_pydatetime())
 
-plt.figure(figsize=(20,10), dpi=80, facecolor='black', edgecolor='cyan')
-
+plt.figure(figsize=(20,10), dpi=80)
 plt.plot(prices.index, prices.price)
 plt.xlabel('date')
 plt.ylabel('price (USD)')
