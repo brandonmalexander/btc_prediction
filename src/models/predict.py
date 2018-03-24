0015df0faeb8d76805b0c8264821cb2ca4f04955
brandon <alexander.bra@husky.neu.edu>
2018-03-24 17:09:37 -0400

    testing vanilla GBM in jupyter

--- src/models/predict.py
+++ src/models/predict.py
@@ -5,13 +5,20 @@ import gbm
 
 seed = 5
 N = 2**6
+
 W = gbm.Brownian(seed, N)[0]
+
+
 
 prices = pd.read_csv('data/price_data.csv', infer_datetime_format=True)
 p = prices['price']
 timefactor = 1109
+
 mu, sigma = gbm.daily_returns(p, timefactor)
 
+
+
 S, xs = gbm.modified_GBM(p[0], mu, sigma, W, N)
+
 plt.plot(xs, S)
 plt.show()
